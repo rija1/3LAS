1c14d4566746c258e6b50c7e7e60dbd28d94e892
rija1 <rija.ratinahirana@gmail.com>
2023-03-25 13:39:43 +0545

    stuff

--- srts/admin/server.js
+++ srts/admin/server.js
@@ -1,12 +1,9 @@
-const { exec } = require('child_process');
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const path = require('path');
 const pm2 = require('pm2')
-
-// const server = http.createServer(app);
 
 const PORT = process.env.PORT || 3000;
 
@@ -17,38 +14,64 @@ app.get('/', (req, res) => {
 
 const processNames = ['channel_1', 'channel_2', 'channel_3', 'channel_4'];
 
+// Start a process
+function startProcess(processName) {
+  pm2.start(processName, (err, proc) => {
+    if (err) {
+      console.error(`Error starting process ${processName}: ${err}`);
+      return;
+    }
+    console.log(`Started process ${processName}`);
+  });
+}
+
+// Stop a process
+function stopProcess(processName) {
+  pm2.stop(processName, (err, proc) => {
+    if (err) {
+      console.error(`Error stopping process ${processName}: ${err}`);
+      return;
+    }
+    console.log(`Stopped process ${processName}`);
+  });
+}
+
+// Restart a process
+function restartProcess(processName) {
+  pm2.restart(processName, (err, proc) => {
+    if (err) {
+      console.error(`Error restarting process ${processName}: ${err}`);
+      return;
+    }
+    console.log(`Restarted process ${processName}`);
+  });
+}
+
+
 function checkPm2ProcessStatus() {
-  exec(`pm2 list`, (err, stdout, stderr) => {
+  pm2.list((err, processList) => {
     if (err) {
       console.error(`Error: ${err}`);
       return;
     }
 
-    const processList = stdout.trim().split('\n').slice(1); // Get list of processes, excluding first line
     const processStatus = {};
 
     for (const processName of processNames) {
 
       // Check if process is present in list
-      const isPresent = processList.some(line => line.includes(processName));
+      const processInfo = processList.find(info => info.name === processName);
 
       // Check if process is running
       let isRunning = false;
-      let processInfo;
-      if (isPresent) {
-        exec(`pm2 describe ${processName}`, (err, stdout, stderr) => {
-          if (err) {
-            console.error(`Error: ${err}`);
-            return;
-          }
-          processInfo = stdout.trim();
-          isRunning = processInfo.includes('online');
-          processStatus[processName] = isRunning;
-          io.emit(`${processName}-status`, isRunning);
+      if (processInfo) {
+        isRunning = processInfo.pm2_env.status === 'online';
+        processStatus[processName] = isRunning;
+        io.emit(`${processName}-status`, isRunning);
 
-          updateElapsedTime(processName);
+        // updateElapsedTime(processName);
+        io.emit(`${processName}-time`, getElapsedTimeFromTimestamp(processInfo.pm2_env.pm_uptime));
 
-        });
       } else {
         processStatus[processName] = false;
         io.emit(`${processName}-status`, false);
@@ -57,21 +80,32 @@ function checkPm2ProcessStatus() {
   });
 }
 
+function getElapsedTimeFromTimestamp(timestamp) {
+  const startDate = new Date(timestamp);
+  const elapsed = Date.now() - startDate.getTime();
+  const seconds = Math.floor(elapsed / 1000) % 60;
+  const minutes = Math.floor(elapsed / 1000 / 60) % 60;
+  const hours = Math.floor(elapsed / 1000 / 60 / 60);
+  return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+}
+
+
 function updateElapsedTime(processName) {
-  
+
   const startTimeCommand = `pm2 jlist | jq '.[] | select(.name == "${processName}") | .pm2_env.pm_uptime'`;
   io.emit(`${processName}-time`, startTimeCommand); return;
-  exec(startTimeCommand, (err, stdout, stderr) => {
+
+  pm2.describe(processName, (err, processInfo) => {
     if (err) {
       console.error(`Error: ${err}`);
       return;
     }
 
-    const startTime = parseFloat(stdout.trim()) * 1000;
+    const startTime = processInfo.pm2_env.pm_uptime;
     const elapsed = Date.now() - startTime;
-    
-    io.emit(`${processName}-time`, formatTime("elapsed"));
-    
+
+    io.emit(`${processName}-time`, formatTime(elapsed));
+
   });
 }
 
@@ -85,18 +119,44 @@ function formatTime(milliseconds) {
 
 function processChecks() {
   checkPm2ProcessStatus();
-  
 }
 
+// Listen for socket.io connections
+io.on('connection', (socket) => {
+
+  // Emit the process status when a client connects
+  checkPm2ProcessStatus();
+
+  // Listen for start/stop events from clients
+  socket.on('start-process', (processName) => {
+    startProcess(processName);
+  });
+
+  socket.on('stop-process', (processName) => {
+    stopProcess(processName);
+  });
+
+  socket.on('restart-process', (processName) => {
+    restartProcess(processName);
+  });
+
+});
 
 http.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
-  
+
   // Send initial status to clients
   // for (const processName of processNames) {
   //   io.emit(`${processName}-status`, false);
   // }
-  
+
   // Send status updates every 1 seconds
-  setInterval(processChecks, 1000);
+
+  // Send status updates every 1 seconds
+  function updateStatus() {
+    processChecks();
+    setTimeout(updateStatus, 1500);
+  }
+  updateStatus();
+
 });
--- srts/server/test.sh
+++ srts/server/test.sh
@@ -1,10 +1,26 @@
 #!/bin/sh
 
 
+
 pm2 start "ffmpeg -fflags +nobuffer+flush_packets -flags low_delay -rtbufsize 64 -probesize 64 -y -f avfoundation -i :3 \
 -af aresample=resampler=soxr -acodec pcm_s16le -ar 48000 -ac 1 \
 -f s16le -fflags +nobuffer+flush_packets -packetsize 384 -flush_packets 1 -bufsize 960 pipe:1 \
-| node 3las.server.js -port 3101 -samplerate 48000 -channels 1" --name 3las_channel1
+| node 3las.server.js -port 3101 -samplerate 48000 -channels 1" --name channel_1
+
+pm2 start "ffmpeg -fflags +nobuffer+flush_packets -flags low_delay -rtbufsize 64 -probesize 64 -y -f avfoundation -i :3 \
+-af aresample=resampler=soxr -acodec pcm_s16le -ar 48000 -ac 1 \
+-f s16le -fflags +nobuffer+flush_packets -packetsize 384 -flush_packets 1 -bufsize 960 pipe:1 \
+| node 3las.server.js -port 3102 -samplerate 48000 -channels 1" --name channel_2
+
+pm2 start "ffmpeg -fflags +nobuffer+flush_packets -flags low_delay -rtbufsize 64 -probesize 64 -y -f avfoundation -i :3 \
+-af aresample=resampler=soxr -acodec pcm_s16le -ar 48000 -ac 1 \
+-f s16le -fflags +nobuffer+flush_packets -packetsize 384 -flush_packets 1 -bufsize 960 pipe:1 \
+| node 3las.server.js -port 3103 -samplerate 48000 -channels 1" --name channel_3
+
+pm2 start "ffmpeg -fflags +nobuffer+flush_packets -flags low_delay -rtbufsize 64 -probesize 64 -y -f avfoundation -i :3 \
+-af aresample=resampler=soxr -acodec pcm_s16le -ar 48000 -ac 1 \
+-f s16le -fflags +nobuffer+flush_packets -packetsize 384 -flush_packets 1 -bufsize 960 pipe:1 \
+| node 3las.server.js -port 3104 -samplerate 48000 -channels 1" --name channel_4
 
 # ffmpeg -fflags +nobuffer+flush_packets -flags low_delay -rtbufsize 64 -probesize 64 -y -f alsa -i hw:0 \
 # -af aresample=resampler=soxr -acodec pcm_s16le -ar 48000 -ac 1 \
